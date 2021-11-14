2049448e3c3b5a7fb33174fd67f7717b59a90a6a
jimmy980189 <jimmy980189@gmail.com>
2021-11-14 23:56:52 +0800

    init

---
+++ hw1/server.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+    
+    return 0;
+}
